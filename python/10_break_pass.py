02dd1654e488c186d728d5980da44b3950c341f1
Byeonsangil <ljlj2010@naver.com>
2025-09-11 11:20:00 +0900

    continue 문 추가

--- python/10_break_pass.py
+++ python/10_break_pass.py
@@ -1,11 +1,20 @@
 cup = 0
 running = True
 
-while True:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        #running = False
-        break
+# while True:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         #running = False
+#         break
 
 print('while 문 종료')
+
+for i in range(1,10):
+    if i == 3:
+        continue
+    if i == 6:
+        continue
+    if i == 9:
+        continue
+    print(i)
