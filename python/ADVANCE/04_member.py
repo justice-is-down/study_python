6e94a4390fb49d36d23b58725cd8199c82db9b48
Byeonsangil <ljlj2010@naver.com>
2025-09-15 14:22:15 +0900

    클래스 초기화

---
+++ python/ADVANCE/04_member.py
@@ -0,0 +1,13 @@
+class Robot:
+
+    # 생성자 ? 객체화 할때 호출되는 함수의 일종으로
+    # 객체화가 될떄 가장 먼저 호출된다.
+
+    def __init__(self):
+        print('Robot이 복사될떄 제일 먼저 호출되는 멤버')
+
+    def doIt(self):
+        print('나는 Robot 의 함수 입니다.')
+
+robot = Robot()
+robot.doIt()
