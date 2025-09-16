c0bdb2f8a0ab8e9e5e447006ae765bef262f28f8
Byeonsangil <ljlj2010@naver.com>
2025-09-16 12:08:15 +0900

    클래스 생성 마무리

---
+++ python/ADVANCE/08_inherit.py
@@ -0,0 +1,19 @@
+class Runner:
+    def run(self):
+        print(f'달린다.')
+
+    def sprint(self):
+        print(f'전력질주를 한다.')
+
+
+class Jumper:
+    def Jump(self):
+        print(f'점프를한다')
+
+    def high_jump(self):
+        print(f'높이 점프를 한다.')
+
+
+class Person(Jumper, Runner): #Jumper 와 Runner를 상속 받았다.
+    def walk(self):
+        print(f'걷는다')
