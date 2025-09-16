d4bbbdfa3465f61ca955056a8f90506dc843a82c
Byeonsangil <ljlj2010@naver.com>
2025-09-16 17:33:47 +0900

    예외처리

--- python/ADVANCE/10_override.py
+++ python/ADVANCE/10_override.py
@@ -3,22 +3,31 @@ class Car:
         print('시동이 걸린다')
 
     def run(self):
-        print('차가 시속50km로 달린다.')
+        class Car:
+            def start(self):
+                print('시동이 걸린다.')
 
-    def stop(self):
-        print('차가 멈춘다.')
+            def run(self):
+                print('차가 시속 50km 로 달린다.')
 
-class MyCar(Car):
+            def stop(self):
+                print('차가 멈춘다.')
 
-    turbo = False
+        class MyCar(Car):
 
+            turbo = False
 
-    def run(self): #부모와 같은 메서드를 사용하면 override로 인식된다.
-        if self.turbo ==True:
-            print('차가 시속 200km로 달린다')
-        else:
-            super().run.() #부모의 run을 그대로 쓰겟다.
-mc = MyCar()
-mc.start()
-mc.run = true
-mc.stop()
+            def run(self):  # 부모와 같은 메서드를 사용하면 override 로 인식된다.
+                if self.turbo == True:
+                    print('차가 시속 200km 로 달린다.')
+                else:
+                    super().run()  # 부모의 run 을 그대로 쓰겠다.
+
+        mc = MyCar()
+        mc.start()
+
+        mc.run()
+        mc.turbo = True
+        mc.run()
+
+        mc.stop()
