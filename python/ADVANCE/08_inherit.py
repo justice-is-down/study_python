9ebc63fcb0fe086faf5ab7eb07d3956d2312dc84
Byeonsangil <ljlj2010@naver.com>
2025-09-16 12:19:32 +0900

    클래스 생성 마무리

--- python/ADVANCE/08_inherit.py
+++ python/ADVANCE/08_inherit.py
@@ -17,3 +17,14 @@ class Jumper:
 class Person(Jumper, Runner): #Jumper 와 Runner를 상속 받았다.
     def walk(self):
         print(f'걷는다')
+
+#walk()함수를 사용하기 위해 Person 클래스를 객체화한다.
+p=Person()
+p.walk()
+#상속받은 함수들을 내것처럼 (p객체로부터) 사용한다.
+#상속을 받으면 좋은점 1.객체 입장에서는 기능이 늘어난다.
+#상속을 받으면 좋은점 2.사용자 입장에서는 하나의 객체화로 여러기능을 사용할수 있따.
+p.Jump()
+p.high_jump()
+p.run()
+p.sprint()
